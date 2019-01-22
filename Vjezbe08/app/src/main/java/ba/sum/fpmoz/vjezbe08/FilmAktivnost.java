92df68ea0b0d27988b3260a5d30b9216d6743050
Daniel Vasić <dvasic1@gmail.com>
2019-01-22 22:09:32 +0100

    Vjezbe 21.01.2019.

---
+++ Vjezbe08/app/src/main/java/ba/sum/fpmoz/vjezbe08/FilmAktivnost.java
@@ -0,0 +1,13 @@
+package ba.sum.fpmoz.vjezbe08;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FilmAktivnost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_film_aktivnost);
+    }
+}
---
+++ Vjezbe08/app/src/main/java/ba/sum/fpmoz/vjezbe08/model/Rating.java
@@ -0,0 +1,4 @@
+package ba.sum.fpmoz.vjezbe08.model;
+
+public class Rating {
+}
